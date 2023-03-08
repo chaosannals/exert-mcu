611e09907e3dd5e05501689639ab89a2018f78fe
chenshenchao <chenshenchao@outlook.com>
2023-03-09 00:26:04 +0800

    save

---
+++ esp32/pydemo/mfrc522_write.py
@@ -0,0 +1,33 @@
+# RFID
+# MFRC522 写
+
+from mfrc522_spi import MFRC522
+
+mfrc = MFRC522(1, sck=18, mosi=23, miso=19, cs=4, rst=2)
+
+try:
+    while True:
+        print('start')
+        (stat, tag_type) = mfrc.request(mfrc.REQIDL)
+        if stat == mfrc.OK:
+            (stat, raw_uid) = mfrc.anticoll()
+            if stat == mfrc.OK:
+                print(f'tag type: {tag_type} raw uid: {raw_uid}')
+                if mfrc.select_tag(raw_uid) == mfrc.OK:
+                    key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,] # 默认密码
+                    if mfrc.auth(mfrc.AUTHENT1A, 8, key, raw_uid) == mfrc.OK:
+                        print(f"address 8 data: {mfrc.read(8)}")
+                        stat = mfrc.write(8, b'\x01\x01\x01\x04\x05\x06\x07\x08\x01\x01\x01\x04\x05\x06\x07\x08')
+                        if stat == mfrc.OK:
+                            print('write OK')
+                        else:
+                            print('write failed')
+                    else:
+                        print('auth failed')
+                else:
+                    print('select failed')
+            else:
+                print('anticoll failed')
+            
+except Exception as e:
+    print(e)
--- esp32/pydemo/sensor_adc.py
+++ esp32/pydemo/sensor_adc.py
@@ -3,12 +3,21 @@
 # 模拟温度传感器 
 # 光敏传感器
 # 火焰报警器
+# 红外循迹传感器 5V
 
 from machine import Pin, ADC
 from time import sleep_ms
 
 adc = ADC(Pin(2))
 adc.atten(ADC.ATTN_11DB) # 11dB 衰减
+
+pin = Pin(4, Pin.IN)
+
+def on_falling(v):
+    print('falling', v)
+
+pin.irq(trigger=Pin.IRQ_FALLING, handler=on_falling)
+
 
 v = adc.read() >> 4
 
