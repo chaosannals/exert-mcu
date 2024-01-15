6565c51158a7941129681d0bd9da687c43a3801c
chaosannals <chaosannals@outlook.com>
2024-01-15 16:06:13 +0800

    save

---
+++ espidfdemo/readme.md
@@ -0,0 +1,3 @@
+# esp-idf demo
+
+参考官方 git 仓库的 examples
---
+++ espidfdemo/spiffsdemo/main/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+// 打印芯片信息
+int print_chip_info(void) {
+    esp_chip_info_t chip_info;
+    uint32_t flash_size;
+    esp_chip_info(&chip_info);
+
+    // 支持的功能，支持就会带上相应的信息。
+    // wifi
+    // BT 蓝牙
+    // BLE
+    // 
+    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+            CONFIG_IDF_TARGET,
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+            (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+            (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+    // 芯片版本
+    unsigned major_rev = chip_info.revision / 100; // 主版本
+    unsigned minor_rev = chip_info.revision % 100; // 次版本
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return -1;
+    }
+
+    // 闪存大小
+    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    // 堆大小
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+    return 0;
+}
+
+void wait_s_restart(int s) {
+    // 等待 10 秒后重启。
+    for (int i = s; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout); // 刷新标准输出
+    esp_restart(); // 重启
+}
+
+void app_main(void)
+{
+    print_chip_info();
+    wait_s_restart(10);
+}
