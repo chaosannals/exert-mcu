eade8a8d63b4d4b8af7360582452e23697e5eaf9
chenshenchao <chenshenchao@outlook.com>
2024-03-20 00:42:29 +0800

    save

--- stm32/f401rct6_joystick/Core/Src/ssd1306.c
+++ stm32/f401rct6_joystick/Core/Src/ssd1306.c
@@ -2,8 +2,9 @@
 #include <string.h>
 #include "stm32f4xx_hal.h"
 
-// 0-7 页， 每 1 页  = 8 列 * 128 行
-// 1 列由一字节表示，  8 列 * 128 行 * 8 页
+// 0-7 页， 每 1 页  = 8 列 * 128 行 = 128 bytes
+// 写的时候选定页地址，写入 128 字节
+// 1 列由1字节表示，  8 列 * 128 行 * 8 页
 
 #define SSD1306_IIC_ADDR0 0x3C << 1
 #define SSD1306_IIC_ADDR1 0x3D << 1
